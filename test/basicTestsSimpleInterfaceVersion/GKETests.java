0b0933e770b09fd4eb0578392ea810ea46c789d8
vova <vilenchik.vladimir@gmail.com>
2019-12-09 14:21:01 +0100

    re-architecturing - back to ASAPJavaApplication interface + removing unnessecary classes

---
+++ test/basicTestsSimpleInterfaceVersion/GKETests.java
@@ -0,0 +1,168 @@
+package basicTestsSimpleInterfaceVersion;
+
+import net.sharksystem.asap.ASAPEngineFS;
+import net.sharksystem.asap.ASAPException;
+import net.sharksystem.asap.apps.*;
+import net.sharksystem.asap.util.ASAPEngineThread;
+import net.sharksystem.cmdline.TCPChannel;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class GKETests {
+    public static final String ALICE = "Alice";
+    public static final String BOB = "Bob";
+    public static final String TESTS_ROOT_FOLDER = "tests/";
+    public static final String ALICE_ROOT_FOLDER = TESTS_ROOT_FOLDER + "Alice";
+    public static final String BOB_ROOT_FOLDER = TESTS_ROOT_FOLDER + "Bob";
+    private static final CharSequence APP_FORMAT = "TEST_FORMAT";
+    private static final byte[] TESTMESSAGE = "Hallo ALice Bob WHatever Bye Bye ".getBytes();
+    private static final int PORT = 7777;
+
+    @Test
+    public void usageTest() throws IOException, ASAPException, InterruptedException {
+        ASAPEngineFS.removeFolder(TESTS_ROOT_FOLDER);
+        Collection<CharSequence> formats = new HashSet<>();
+        formats.add(APP_FORMAT);
+
+        ASAPJavaApplication asapJavaApplicationAlice =
+                ASAPJavaApplicationFS.createASAPJavaApplication(ALICE, ALICE_ROOT_FOLDER, formats);
+
+        Collection<CharSequence> recipients = new HashSet<>();
+        recipients.add(BOB);
+
+        asapJavaApplicationAlice.sendASAPMessage(APP_FORMAT, "yourSchema://yourURI", recipients, TESTMESSAGE);
+        asapJavaApplicationAlice.setASAPMessageReceivedListener(APP_FORMAT, new ListenerExample());
+
+        // create bob engine
+        ASAPJavaApplication asapJavaApplicationBob =
+                ASAPJavaApplicationFS.createASAPJavaApplication(BOB, BOB_ROOT_FOLDER, formats);
+
+        ListenerExample listenerBob = new ListenerExample();
+        asapJavaApplicationBob.setASAPMessageReceivedListener(APP_FORMAT, listenerBob);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        //                                        create a tcp connection                                //
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // create connections for both sides
+        TCPChannel aliceChannel = new TCPChannel(PORT, true, "a2b");
+        TCPChannel bobChannel = new TCPChannel(PORT, false, "b2a");
+
+        aliceChannel.start(); bobChannel.start();
+        // wait to connect
+        aliceChannel.waitForConnection(); bobChannel.waitForConnection();
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        //                                        run asap session                                       //
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // run engine as thread
+        ASAPHandleConnectionThread aliceEngineThread = new ASAPHandleConnectionThread(asapJavaApplicationAlice,
+                aliceChannel.getInputStream(), aliceChannel.getOutputStream());
+
+        aliceEngineThread.start();
+
+        // let's start communication
+        asapJavaApplicationBob.handleConnection(bobChannel.getInputStream(), bobChannel.getOutputStream());
+
+        // wait until communication probably ends
+        Thread.sleep(2000); System.out.flush(); System.err.flush();
+        // close connections: note ASAPEngine does NOT close any connection!!
+        aliceChannel.close(); bobChannel.close(); Thread.sleep(1000);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        //                                            test results                                       //
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // received?
+        Assert.assertTrue(listenerBob.hasReceivedMessage());
+    }
+
+    @Test
+    public void onlineTest() throws IOException, ASAPException, InterruptedException {
+        ASAPEngineFS.removeFolder(TESTS_ROOT_FOLDER);
+
+        Collection<CharSequence> formats = new HashSet<>();
+        formats.add(APP_FORMAT);
+
+        // create alice engine
+        ASAPJavaApplication asapJavaApplicationAlice =
+                ASAPJavaApplicationFS.createASAPJavaApplication(ALICE, ALICE_ROOT_FOLDER, formats);
+
+        // create bob engine
+        ASAPJavaApplication asapJavaApplicationBob =
+                ASAPJavaApplicationFS.createASAPJavaApplication(BOB, BOB_ROOT_FOLDER, formats);
+
+        // create Bob receiver
+        ListenerExample listenerBob = new ListenerExample();
+        asapJavaApplicationBob.setASAPMessageReceivedListener(APP_FORMAT, listenerBob);
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        //                                        create a tcp connection                                //
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // create connections for both sides
+        TCPChannel aliceChannel = new TCPChannel(PORT, true, "a2b");
+        TCPChannel bobChannel = new TCPChannel(PORT, false, "b2a");
+
+        aliceChannel.start(); bobChannel.start();
+        // wait to connect
+        aliceChannel.waitForConnection(); bobChannel.waitForConnection();
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        //                                        run asap session                                       //
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // run engine as thread
+        ASAPHandleConnectionThread aliceEngineThread = new ASAPHandleConnectionThread(asapJavaApplicationAlice,
+                aliceChannel.getInputStream(), aliceChannel.getOutputStream());
+
+        aliceEngineThread.start();
+
+        // let's start communication
+        asapJavaApplicationBob.handleConnection(bobChannel.getInputStream(), bobChannel.getOutputStream());
+
+        // nothing really spectaculare has happend until now
+
+        // create a message and send message from alice to bob over an existing connection
+        Collection<CharSequence> recipients = new HashSet<>();
+        recipients.add(BOB);
+      asapJavaApplicationAlice.sendASAPMessage(APP_FORMAT, "yourSchema://yourURI", recipients, TESTMESSAGE);
+
+        // wait until communication probably ends
+        Thread.sleep(2000); System.out.flush(); System.err.flush();
+        // close connections: note ASAPEngine does NOT close any connection!!
+        aliceChannel.close(); bobChannel.close(); Thread.sleep(1000);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        //                                            test results                                       //
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // received?
+        Assert.assertTrue(listenerBob.hasReceivedMessage());
+    }
+
+    private class ListenerExample implements ASAPMessageReceivedListener {
+
+        private boolean hasReceivedMessage = false;
+
+        @Override
+        public void asapMessagesReceived(ASAPMessages messages) {
+            try {
+                System.out.println("#message == " + messages.size());
+                this.hasReceivedMessage = true;
+            } catch (IOException e) {
+                // do something with it.
+            }
+        }
+
+        public boolean hasReceivedMessage() {
+            return this.hasReceivedMessage;
+        }
+    }
+}
